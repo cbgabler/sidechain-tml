5a012d453fbeccaeabb3fca86509cf278737d69b
Carson Gabler <carsongabler7@gmail.com>
2026-05-13 16:58:33 -0700

    feat: Add inital harness script, header files
    Signed-off-by: Carson Gabler <carsongabler7@gmail.com>

--- hardware/esp/harness.ino
+++ hardware/esp/harness.ino
@@ -1,1 +1,26 @@
 #include <Arduino.h>
+
+#include <scripts/anomaly.h>
+#include <scripts/baseline.h>
+#include <scripts/memory.h>
+
+void setup() {
+    Serial.begin(115200);
+
+
+}
+
+void loop() {
+    Serial.print("Time: ");
+    uint32_t time = micros();
+    baseline_task();
+    uint32_t elapsed = micros() - time;
+
+    Serial.println(time);
+
+    uint32_t heap = esp_get_free_heap_size();
+    
+    Serial.printf("T:%u H:%u R:%d\n", elapsed, heap, reset_count);
+
+    delay(100);
+}
